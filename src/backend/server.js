43d3bc24e3f50da3931f937a7caf78fc5d4db2e2
MatheusVicenteSantosSilva <matheuzinhovss@gmail.com>
2025-10-29 00:00:02 -0300

    Update server.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -3,14 +3,20 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config(); 
 
+// Configurações do banco de dados Clever Cloud
+process.env.MYSQL_ADDON_HOST = "bxmtixqywgoapzy8nyei-mysql.services.clever-cloud.com";
+process.env.MYSQL_ADDON_DB = "bxmtixqywgoapzy8nyei";
+process.env.MYSQL_ADDON_USER = "u02vstkj2idmhuw7";
+process.env.MYSQL_ADDON_PORT = "3306";
+process.env.MYSQL_ADDON_PASSWORD = "oXpo6JIPPlQk9aAvg62i";
+process.env.MYSQL_ADDON_URI = "mysql://u02vstkj2idmhuw7:oXpo6JIPPlQk9aAvg62i@bxmtixqywgoapzy8nyei-mysql.services.clever-cloud.com:3306/bxmtixqywgoapzy8nyei";
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Importar rotas
 const authRoutes = require('./routes/auth'); // PRECISA SER CRIADA!
 const transactionRoutes = require('./routes/transactions');
-
-// ... (Restante dos imports e config de PORT)
 
 // Middlewares
 app.use(cors({
